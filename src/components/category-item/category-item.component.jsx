f44730cc5c6c9906b345c0e0362b6e1d742ce3e1
aayush agrawal <aayush.ag85@gmail.com>
2022-10-30 14:35:28 -0400

    All local development

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,20 @@
+import './category-item.style.scss';
+
+
+const Category=({category})=>{
+    const { title, imageUrl}=category;
+return(
+    <div className="category-container">
+            <div className="background-image" style={{
+              backgroundImage: `url(${imageUrl})`,
+            }} />
+            <div  className="category-body-container">
+              <h2>{title}</h2>
+              <p>SHOP NOW</p>
+            </div>
+          </div>
+);
+
+}
+
+export default Category;
